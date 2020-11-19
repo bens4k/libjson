ead6d6c74ac91c3cd08fddb867cafc2a6ffa1b84
ab_ben <abdelkarim128@gmail.com>
2020-11-19 19:00:45 +0100

    func prototype different from declaration
    joinwstr -> joinstr

--- combined/json.c
+++ combined/json.c
@@ -123,7 +123,7 @@ char char_to_digit(const char c);
 char digit_to_char(const char d);
 
 CString *join(const CString *cs1, const CString *cs2, char delimiter);
-CString *joinwstr(const CString *cs, char* s, char delimiter);
+CString *joinstr(const CString *cs, char* s, char delimiter);
 CString *enclose(const CString *cs, char c1, char c2);
 CString *lowercase(const CString *cs);
 CString *unescape_special_chars(const CString *src_cs);
